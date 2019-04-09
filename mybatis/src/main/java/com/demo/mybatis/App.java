0884ee6298b0b2a264526d5d8ce2cbbecf991e86
wanggang <wanggang-bj@fang.com>
2019-04-09 17:20:43 +0800

    添加

---
+++ mybatis/src/main/java/com/demo/mybatis/App.java
@@ -0,0 +1,13 @@
+package com.demo.mybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
