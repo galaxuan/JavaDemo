7eafaee610be7ea2a58dbe67deecd7c8869f95f2
wanggang <wanggang-bj@fang.com>
2019-05-17 18:39:01 +0800

    添加

---
+++ springdemo/src/main/java/com/demo/springdemo/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.demo.springdemo.dao;
+
+/**
+ * @program: javademo
+ * @description: UserDao
+ * @author: wang gang
+ * @create: 2019-05-17 18:08
+ */
+public interface UserDao {
+    void add();
+}
---
+++ springdemo/src/main/java/com/demo/springdemo/dao/UserDaoImpl.java
@@ -0,0 +1,14 @@
+package com.demo.springdemo.dao;
+
+/**
+ * @program: javademo
+ * @description:
+ * @author: wang gang
+ * @create: 2019-05-17 18:11
+ */
+public class UserDaoImpl implements UserDao {
+    @Override
+    public void add() {
+        System.out.println("添加用户");
+    }
+}
