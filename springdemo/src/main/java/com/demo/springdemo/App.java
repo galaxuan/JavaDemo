e1f5c7f3592541b721d094afb238cb7395910e38
wanggang <wanggang-bj@fang.com>
2019-05-20 15:35:21 +0800

    修改

--- springdemo/src/main/java/com/demo/springdemo/App.java
+++ springdemo/src/main/java/com/demo/springdemo/App.java
@@ -1,13 +1,19 @@
 package com.demo.springdemo;
 
+import com.demo.springdemo.dao.UserDaoImpl;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.demo.springdemo");
+
+        UserDaoImpl daoImpl=applicationContext.getBean("UserDaoImpl",UserDaoImpl.class);
+
+        System.out.println(daoImpl);
+        applicationContext.close();
     }
 }
--- springdemo/src/main/java/com/demo/springdemo/dao/UserDaoImpl.java
+++ springdemo/src/main/java/com/demo/springdemo/dao/UserDaoImpl.java
@@ -1,11 +1,15 @@
 package com.demo.springdemo.dao;
 
+import org.springframework.stereotype.Repository;
+
 /**
  * @program: javademo
  * @description:
  * @author: wang gang
  * @create: 2019-05-17 18:11
  */
+
+@Repository
 public class UserDaoImpl implements UserDao {
     @Override
     public void add() {
