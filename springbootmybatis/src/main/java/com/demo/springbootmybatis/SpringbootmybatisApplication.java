73d1cbf11e3341af97061c1e2c53f1d8bddb720d
wanggang <wanggang-bj@fang.com>
2019-05-20 19:16:26 +0800

    修改

--- springbootmybatis/src/main/java/com/demo/springbootmybatis/SpringbootmybatisApplication.java
+++ springbootmybatis/src/main/java/com/demo/springbootmybatis/SpringbootmybatisApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 @MapperScan("com.demo.springbootmybatis.mapper")
 public class SpringbootmybatisApplication {
@@ -11,6 +12,20 @@ public class SpringbootmybatisApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(SpringbootmybatisApplication.class, args);
+        /*
+        ConfigurableApplicationContext context = SpringApplication.run(SpringbootmybatisApplication.class, args);
+
+        DataSource dataSource = context.getBean(DataSource.class);
+
+        System.out.println("dataSource:" + dataSource);
+        System.out.println("=====================");
+        EmpMapper mapper = context.getBean(EmpMapper.class);
+
+        Emp emp = mapper.findById(7566);
+        System.out.println("emp:" + emp);
+
+        context.close();
+        */
     }
 
 }
