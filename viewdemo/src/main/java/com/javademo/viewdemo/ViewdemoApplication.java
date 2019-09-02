4deac00eb17c2e382cb7ad6aacb81124c7573207
wanggang <wanggang-bj@fang.com>
2019-09-02 22:58:56 +0800

    修改

---
+++ viewdemo/src/main/java/com/javademo/viewdemo/ViewdemoApplication.java
@@ -0,0 +1,13 @@
+package com.javademo.viewdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewdemoApplication.class, args);
+    }
+
+}
