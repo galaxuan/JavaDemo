ac9f4b68fc7e4eed9e1dbc4c178acade4c24602c
wanggang-bj <wanggang-bj@fang.com>
2022-06-24 09:35:12 +0800

    1

---
+++ demo/src/main/java/com/demo/entity/Student.java
@@ -0,0 +1,48 @@
+package com.demo.entity;
+
+public class Student {
+    private String name;
+
+    private String grade;
+
+    private Double score;
+
+    public Student(String name, String grade, Double score) {
+        this.name = name;
+        this.grade = grade;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", grade=" + grade +
+                ", score=" + score +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+}
--- demo/src/test/java/com/demo/LambdaTest.java
+++ demo/src/test/java/com/demo/LambdaTest.java
@@ -1,16 +1,39 @@
 package com.demo;
 
 import com.demo.entity.Person;
-import jdk.nashorn.internal.runtime.regexp.joni.constants.CCSTATE;
+import com.demo.entity.Student;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
 
 @SpringBootTest
 public class LambdaTest {
+
+    public void Test() {
+        File f = new File(".");
+        File[] files0 = f.listFiles((File file, String name) -> {
+            if (name.endsWith(".txt")) {
+                return true;
+            }
+            return false;
+        });
+
+        File[] files1 = f.listFiles((File file, String name) -> {
+            return name.endsWith(".txt");
+        });
+
+        File[] files2 = f.listFiles((file, name) -> name.endsWith(".txt"));
+    }
 
     List<Person> peoples = Arrays.asList(
             new Person(1, "张学友", 23, 5000),
@@ -19,9 +42,17 @@ public class LambdaTest {
             new Person(4, "黎明", 28, 6000)
     );
 
+    List<Student> students = Arrays.asList(new Student[]{
+            new Student("zhangsan", "1", 91d),
+            new Student("lisi", "2", 89d),
+            new Student("wangwu", "1", 50d),
+            new Student("zhaoliu", "2", 78d),
+            new Student("sunqi", "1", 59d)});
+
     @Test
     public void test0() {
         peoples.stream().filter(s -> s.getSalary() > 4000).forEach(System.out::println);
+        List<Integer> list = peoples.stream().filter(s -> s.getSalary() > 3000).map(s -> s.getSalary()).collect(Collectors.toList());
     }
 
     @Test
@@ -53,7 +84,7 @@ public class LambdaTest {
     }
 
     /**
-     * Consumer<T> 消费性接口
+     * Consumer<T> 消费型接口
      */
     @Test
     public void test4() {
@@ -65,7 +96,7 @@ public class LambdaTest {
     }
 
     /**
-     * Supplier<T> 供给性接口
+     * Supplier<T> 供给型接口
      */
     @Test
     public void test5() {
@@ -88,30 +119,91 @@ public class LambdaTest {
     }
 
     /**
-     * Function<T, R> 接口
+     * Function<T, R> 函数型接口
      */
     @Test
     public void test6() {
+        String str = strHandler("\t\t\t 中国人牛逼！", s -> s.trim());
+        System.out.println(str);
+    }
+
+    private String strHandler(String str, Function<String, String> fun) {
+        return fun.apply(str);
+    }
+
+    /**
+     * Predicate<T> 断言型接口
+     */
+    @Test
+    public void test7() {
+        List<String> list = Arrays.asList("hello", "fang", "ok", "test");
+
+        List<String> filterList = filterStr(list, s -> s.length() > 3);
+
+        for (String s : filterList) {
+            System.out.println(s);
+        }
 
     }
+
+    private List<String> filterStr(List<String> list, Predicate<String> predicate) {
+        List<String> stringList = new ArrayList<>();
+
+        for (String str : list) {
+            if (predicate.test(str)) {
+                stringList.add(str);
+            }
+        }
+
+        return stringList;
+    }
+
+    @Test
+    public void test8() {
+        peoples.sort(Comparator.comparing(Person::getSalary).reversed().thenComparing(Person::getName));
+
+        for (Person people : peoples) {
+            System.out.println(people.getName());
+        }
+    }
+
+    @Test
+    public void test9() {
+        //按年级分组
+        Map<String, List<Student>> groups = students.stream().collect(Collectors.groupingBy(Student::getGrade));
+
+        System.out.println(groups);
+
+        //统计每个年级的学生数
+        Map<String, Long> gradeCountMap = students.stream().collect(groupingBy(Student::getGrade, counting()));
+
+        System.out.println(gradeCountMap);
+
+        //统计一个单词流中每个单词的个数，按出现顺序排序
+        Map<String, Long> wordCountMap = Stream.of("hello", "world", "abc", "hello")
+                .collect(groupingBy(Function.identity(), LinkedHashMap::new, counting()));
+
+        System.out.println(wordCountMap);
+
+        //获取每个年级最高分的一个学生
+        Map<String, Optional<Student>> topStudentMap = students.stream()
+                .collect(groupingBy(Student::getGrade, maxBy(Comparator.comparing(Student::getScore))));
+
+        Map<String, Student> topStudentMap2 = students.stream()
+                .collect(groupingBy(Student::getGrade, collectingAndThen(maxBy(Comparator.comparing(Student::getScore)), Optional::get)));
+
+        System.out.println(topStudentMap2);
+
+        //按年级统计学生分数信息
+        Map<String, DoubleSummaryStatistics> gradeScoreStat = students.stream()
+                .collect(groupingBy(Student::getGrade, summarizingDouble(Student::getScore)));
+
+        System.out.println(gradeScoreStat);
+
+        //分组内的map
+        Map<String, List<String>> gradeNameMap = students.stream()
+                .collect(groupingBy(Student::getGrade, mapping(Student::getName, toList())));
+
+        System.out.println(gradeNameMap);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
